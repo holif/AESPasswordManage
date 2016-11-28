dcf6945b2718eadfd93c61aa52df7561a6d73c7c
holif <1301297909@qq.com>
2016-11-28 16:22:30 +0800

    注册成功浏览器打开该页面

---
+++ cloudkey/success.php
@@ -0,0 +1,21 @@
+<!doctype html> 
+<html lang="en" class="no-js"> 
+<head> 
+  <meta charset="utf-8" /> 
+  <title>用户 注册 反馈</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style2.css" />
+  <script type='text/javascript' src='js/jquery-1.11.1.min.js'></script>
+  <script type='text/javascript' src='js/jquery.particleground.min.js'></script>
+  <script type='text/javascript' src='js/demo.js'></script>
+</head> 
+<body>
+<div id="particles">
+	<div class="intro">
+		<h1>Now Successful !</h1>
+		<p>在这里，我们基于工业级加密算法和图形化模糊数据，帮助您保存密码。</p>
+		<a  class="btn">注册反馈</a>
+	</div>
+</div>
+</body> 
+</html> 
