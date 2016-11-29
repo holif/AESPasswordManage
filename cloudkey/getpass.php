08bbd0abfca5d21ed91af98127a46a05a8f4e2db
holif <1301297909@qq.com>
2016-11-29 17:59:46 +0800

    update bug

--- cloudkey/getpass.php
+++ cloudkey/getpass.php
@@ -6,8 +6,10 @@
 	$password=$_GET['password'];//登录密码
 	$key=md5($_GET['key']);
 	
-	$status = CheckUser($username,$password);
+	$status = CheckUser($name,$password);
+	
 	$log = "\r\n".date('Y-m-d H:i:s')."	"."parsed password ";
+	
 	if($status == 3)
 	{
 		$num=$_GET['num'];
@@ -15,18 +17,26 @@
 		$file=fopen($dir, 'r');
 		$mima=fread($file, filesize($dir));
 		fclose($file);
-		echo $mima;
 		
 		$log = $log."success.";
+		writelog($name,$log);
+		echo $mima;
 	} elseif($status == 2){
 		$log = $log."fail:password error.";
+		writelog($name,$log);
 		echo "error:pass error";
+		exit;
 	} else {
+		$log = $log."error:no user";
+		writelog($name,$log);
 		echo "error:no user";
 		exit;
 	}
 
-	$fh=fopen("./total_data/".$name."/cloudkey.log","a");
-	fwrite($fh,$log);
-	fclose($fh);
+	function writelog($name,$log){
+		$fh=fopen("./total_data/".$name."/cloudkey.log","a");
+		fwrite($fh,$log);
+		fclose($fh);		
+	}
+	
 ?>
